76e9a42c55c3a50f599fc109dc9765ca79bdd288
Jared Prather <pratherjared@gmail.com>
2026-02-17 21:28:19 -0600

    feat: Bumps dependencies

--- test/helper.js
+++ test/helper.js
@@ -1,37 +1,19 @@
 // This file contains code that we reuse
 // between our tests.
 
-import helper from 'fastify-cli/helper.js'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const AppPath = path.join(__dirname, '..', 'app.js')
-
-// Fill in this config with all the configurations
-// needed for testing the application
-function config () {
-  return {}
-}
+import Fastify from 'fastify'
+import app from '../app.js'
 
 // automatically build and tear down our instance
 async function build (t) {
-  // you can set all the options supported by the fastify CLI command
-  const argv = [AppPath]
-
-  // fastify-plugin ensures that all decorators
-  // are exposed for testing purposes, this is
-  // different from the production setup
-  const app = await helper.build(argv, config())
+  const fastify = Fastify()
+  await fastify.register(app)
+  await fastify.ready()
 
   // tear down our app after we are done
-  t.teardown(app.close.bind(app))
+  t.teardown(() => fastify.close())
 
-  return app
+  return fastify
 }
 
-export {
-  config,
-  build
-}
+export { build }
